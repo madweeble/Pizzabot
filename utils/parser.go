effc872e57a4435599bd018a462df71a642ddb0f
Kevin Bradley <madweeble@gmail.com>
2020-11-09 01:22:05 +0000

    Added check for grid size

--- utils/parser.go
+++ utils/parser.go
@@ -9,21 +9,40 @@ import (
 	"strings"
 )
 
+var (
+	gridX = 0
+	gridY = 0
+)
+
 // GetCoords will accept the input arguments and
-// return an array of x,y co-ordinates
+// return the grid size and an array of x,y co-ordinates
 func GetCoords(inputString []string) (string, [][2]int) {
 	// convert input args to string and validate
 	argsString := strings.Join(inputString[1:], "")
 	if !validateArgs(argsString) {
 		log.Fatal(errors.New(fmt.Sprintf("ERROR: incorrect arguments, expected: "+
-			"'grid (co-ordinates_1) (co-ordinates_n)', got: %v", argsString)))
+			"'grid_size (co-ordinates_1) ... (co-ordinates_n)', got: %v", argsString)))
 	}
 
 	args := strings.SplitN(argsString, " ", 2)
+	gridX, gridY = setGrid(args[0])
 	coordStr := strings.Join(args[1:], "")
 	coordArray := getCoords(coordStr)
 
 	return args[0], coordArray
+}
+
+func setGrid(grid string) (int, int) {
+	sizes := strings.SplitN(grid, "x", 2)
+	x, err := strconv.Atoi(sizes[0])
+	if err != nil {
+		log.Fatalf("ERROR: failed to parse '%v' into grid size for 'x'", sizes[0])
+	}
+	y, err := strconv.Atoi(sizes[1])
+	if err != nil {
+		log.Fatalf("ERROR: failed to parse '%v' into grid size for 'y'", sizes[1])
+	}
+	return x, y
 }
 
 func getCoords(argsString string) [][2]int {
@@ -35,16 +54,22 @@ func getCoords(argsString string) [][2]int {
 
 	if numOfCoords%2 != 0 {
 		log.Fatalf("we require a matching number of x and y co-ordinates,"+
-			" but there were %v total co-ordinates", numOfCoords)
+			" but there were %v total co-ordinates: %v", numOfCoords, newCoords)
 	}
 
 	locations := make([][2]int, numOfCoords/2)
 	arrayCounter := 0
 	for nextLocation := 0; nextLocation < numOfCoords/2; nextLocation++ {
 		x, _ := strconv.Atoi(newCoords[arrayCounter])
+		if x > gridX {
+			log.Fatalf("ERROR: 'x'-value [%v] is outside the expected range [%v]", x, gridX)
+		}
 		locations[nextLocation][0] = x
 		arrayCounter++
 		y, _ := strconv.Atoi(newCoords[arrayCounter])
+		if y > gridY {
+			log.Fatalf("ERROR: 'y'-value [%v] is outside the expected range [%v]", y, gridY)
+		}
 		locations[nextLocation][1] = y
 		arrayCounter++
 	}
--- utils/parser_test.go
+++ utils/parser_test.go
@@ -5,6 +5,62 @@ import (
 	"testing"
 )
 
+func TestGetCoords(t *testing.T) {
+	type args struct {
+		inputString []string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  string
+		want1 [][2]int
+	}{
+		{"Test1", args{[]string{"./pizzabot", "5x5 ", "(1, 3)"}}, "5x5", [][2]int{{1,3}}},
+		{"Test2", args{[]string{"./pizzabot", "5x5 ", "(1, 3)", "(4, 4)"}}, "5x5", [][2]int{{1,3}, {4,4}}},
+		{"Test3", args{[]string{"./pizzabot", "15x10 ", "(1, 3)", "(4, 4)", "(12, 1)"}}, "15x10", [][2]int{{1,3}, {4,4}, {12, 1}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := GetCoords(tt.args.inputString)
+			if got != tt.want {
+				t.Errorf("GetCoords() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("GetCoords() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_setGrid(t *testing.T) {
+	type args struct {
+		grid string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  int
+		want1 int
+	}{
+		{"0x0", args{"0x0"}, 0, 0},
+		{"5x1", args{"5x1"}, 5, 1},
+		{"1x5", args{"1x5"}, 1, 5},
+		{"5x5", args{"5x5"}, 5, 5},
+		{"10x15", args{"10x15"}, 10, 15},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := setGrid(tt.args.grid)
+			if got != tt.want {
+				t.Errorf("setGrid() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("setGrid() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
 func Test_getCoords(t *testing.T) {
 	type args struct {
 		argsString string
@@ -13,14 +69,14 @@ func Test_getCoords(t *testing.T) {
 		name    string
 		args    args
 		want    [][2]int
-		wantErr bool
 	}{
-		{"Valid x 1", args{"(0, 0)"}, [][2]int{{0, 0}}, false},
-		{"Valid x 2", args{"(1, 3), (4, 4)"}, [][2]int{{1, 3}, {4, 4}}, false},
-		{"Valid x 3", args{"(3, 1), (4, 4), (12, 7)"}, [][2]int{{3, 1}, {4, 4}, {12, 7}}, false},
+		{"Valid x 1", args{"(0, 0)"}, [][2]int{{0, 0}}},
+		{"Valid x 2", args{"(1, 3), (4, 4)"}, [][2]int{{1, 3}, {4, 4}}},
+		{"Valid x 3", args{"(3, 1), (4, 4), (12, 7)"}, [][2]int{{3, 1}, {4, 4}, {12, 7}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			gridX, gridY = 12, 12
 			if got := getCoords(tt.args.argsString); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("getCoords() got = %v, want %v", got, tt.want)
 			}
@@ -41,6 +97,9 @@ func Test_validateArgs(t *testing.T) {
 		{"Valid 1 location", args{"3x3(3,1)"}, true},
 		{"Invalid location", args{"1x1(3)"}, false},
 		{"Invalid grid size", args{"5(1, 3)"}, false},
+		{"Negative x", args{"5x5 (-1, 3)"}, false},
+		{"Negative y", args{"5x5 (1, -3)"}, false},
+		{"Negative xy", args{"5x5 (-1, -3)"}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
